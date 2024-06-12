66dc4bf0c2bb4aa246ab23dc9321cd1f42340ef2
Sophie2201 <sophie.v.a.o.22@gmail.com>
2024-06-12 13:53:15 +0000

    Quem sou eu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Pai eu te amo muito, o senhor é o melhor pai que alguém pode ter ❤️</h1>
+    return <><h1>Quem sou eu? </h1>
+    <h2>
+    <ul>
+        <li>Meu nome é Sophie</li>
+        <li>Tenho 10 anos</li>
+        <li>Nasci em 22/01/2014</li>
+        <li>Qundo crescer quero ser Cirugiã Plastica</li>
+    </ul>
+    </h2></>
 }
 
 export default Home;
