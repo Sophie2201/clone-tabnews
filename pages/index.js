b32e3dd49997178e43cc58be10b8cabc7ee44f3a
Sophie2201 <sophie.v.a.o.22@gmail.com>
2025-09-03 18:36:03 +0000

    adicionando pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home() {
+    return <h1>Lu, tu é o meu sol que me ilumina todos os dias, se você é minha miguinha mande um 
+        audio imitando um porco 🐽 </h1>
+}
+
+export default Home;
