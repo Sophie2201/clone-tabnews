1f002c8e7e724a4735d542f72ea77a92c32138b8
Sophie2201 <sophie.v.a.o.22@gmail.com>
2025-09-17 17:57:34 +0000

    adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1>Lu, tu é o meu sol que me ilumina todos os dias, se você é minha miguinha mande um 
-        audio imitando um porco 🐽 </h1>
+  return (
+    <h1>
+      Lu, tu é o meu sol que me ilumina todos os dias, se você é minha miguinha
+      mande um audio imitando um porco 🐽{" "}
+    </h1>
+  );
 }
 
 export default Home;
