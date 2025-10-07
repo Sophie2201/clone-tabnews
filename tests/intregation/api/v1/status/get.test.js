e023b5e339670f4037e6cd476fde240c15996aa1
Sophie2201 <sophie.v.a.o.22@gmail.com>
2025-10-07 19:17:35 +0000

    Corrige fetch para usar baseUrl dinâmica

--- tests/intregation/api/v1/status/get.test.js
+++ tests/intregation/api/v1/status/get.test.js
@@ -1,5 +1,9 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? `https://${process.env.VERCEL_URL}`
+      : process.env.BASE_URL || "http://localhost:3000";
+  const response = await fetch(`${baseUrl}/api/v1/status`);
   expect(response.status).toBe(200);
   const responseBody = await response.json();
   console.log(responseBody);
